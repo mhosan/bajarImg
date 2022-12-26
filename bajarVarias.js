20a1c3681d7045d9508e0b91196124c44d722a86
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2022-12-26 19:21:49 -0300

    modif para bajar img georef. No anduvo

--- bajarVarias.js
+++ bajarVarias.js
@@ -30,18 +30,18 @@ for (let i = 0; i < 5; i++) {
     const latitude = -34.9213; // Latitud de La Plata, Argentina
     const longitude = -57.9535; // Longitud de La Plata, Argentina
     const distance = 25000; // Distancia entre imágenes en metros
-    const zoom = 19; // Nivel de zoom de la imagen
+    const zoom = 17; // Nivel de zoom de la imagen
     const dimensions = 800; // Dimensiones de la imagen en píxeles
     if (i === 0) {
-        const imageUrl = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${longitude},${latitude},${zoom}/${dimensions}x${dimensions}@2x?access_token=${accessToken}`;
-        downloadImage(imageUrl, `imagen-satelital-${i}.png`);
+        const imageUrl = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${longitude},${latitude},${zoom}/${dimensions}x${dimensions}@2x?access_token=${accessToken}&layers=building-label&format=jpeg`;
+        downloadImage(imageUrl, `imagen-satelital-${i}.jpeg`);
     } else {
         // Calculamos la nueva latitud
         const newLatitude = latitude + (i / 100000) * (distance / 111.111);
         // Calculamos la nueva longitud
         const newLongitude = longitude + (i / 100000) * (distance / (111.111 * Math.cos(latitude)));
-        const imageUrl = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${newLongitude},${newLatitude},${zoom}/${dimensions}x${dimensions}@2x?access_token=${accessToken}`;
-        downloadImage(imageUrl, `imagen-satelital-${i}.png`);
+        const imageUrl = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${newLongitude},${newLatitude},${zoom}/${dimensions}x${dimensions}@2x?access_token=${accessToken}&layers=building-label&format=jpeg`;
+        downloadImage(imageUrl, `imagen-satelital-${i}.jpeg`);
     }
     
 }
