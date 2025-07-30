943290a5156bfa5bc891f345aaa7692ab75de2c0
Marcelo Hosan <mhosan@gmail.com>
2025-07-30 10:23:08 -0300

    replace request por axios

--- bajarImg.js
+++ bajarImg.js
@@ -1,24 +1,31 @@
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 
-// Tu token de acceso de Mapbox
+// Token de acceso de Mapbox
 const accessToken = 'pk.eyJ1IjoibWhvc2FuIiwiYSI6ImNsYjdjd3hvcDA0NDkzcXBreXkwbWI0MzYifQ.CQRqNMLyy56TFpKGdZC9zA';
 
-// La coordenada de la imagen satelital que quieres descargar
-const longitude = -57.9535;
+// La coordenada de la imagen satelital a descargar
+const longitude = -57.9005;
 const latitude = -34.9213;
 
-// La URL de la API de Mapbox que usaremos para descargar la imagen
-const url = `https://api.mapbox.com/v4/mapbox.satellite/${longitude},${latitude},15/800x800.png?access_token=${accessToken}`;
+// La URL de la API de Mapbox 
+const url = `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/${longitude},${latitude},15,0,0/800x800?access_token=${accessToken}`;
 
 // La función que descarga la imagen y la guarda en un archivo
-function downloadImage(url, fileName) {
-  request.get(url)
-    .on('error', (err) => {
-      console.log(err);
-    })
-    .pipe(fs.createWriteStream(fileName));
+async function downloadImage(url, fileName) {
+  try {
+    const response = await axios({
+      url,
+      responseType: 'stream'
+    });
+    response.data.pipe(fs.createWriteStream(fileName));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// Usamos la función downloadImage para descargar la imagen y guardarla en un archivo
-downloadImage(url, 'imagen-satelital.png');
+// downloadImage para descargar la imagen y guardarla en un archivo
+downloadImage(url, 'imagen-satelital.png').then(() => {
+  const metadata = `Fecha: ${new Date().toISOString()}\nLongitud: ${longitude}\nLatitud: ${latitude}`;
+  fs.writeFileSync('imagen-satelital.txt', metadata);
+});
